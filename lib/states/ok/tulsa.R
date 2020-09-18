d76d539b1a51c94fa7179fe13d04e1b9cb97f867
Johann Gaebler <johann.gaebler@gmail.com>
2020-09-17 22:24:51 -0700

    Progress.

--- lib/states/ok/tulsa.R
+++ lib/states/ok/tulsa.R
@@ -6,8 +6,31 @@ source("common.R")
 # to be on the right order of magnitude relative to the number of calls for
 # service.
 load_raw <- function(raw_data_dir, n_max) {
-  d <- load_years(raw_data_dir, n_max = n_max)
-  bundle_raw(d$data, d$loading_problems)
+  # NOTE: Here, "old" refers to second-wave OPP (Dan and Amy, 2018 and 2019),
+  # and "new" refers to the updated data gathered by Phoebe in 2020.
+  d_old <- load_regex(raw_data_dir, "^37_", n_max = n_max)
+  d_new <- load_single_file(
+    raw_data_dir,
+    "trafficcitations_2018to06302020.csv",
+    n_max = n_max
+  )
+  # NOTE: The new column names differ, but the contents are the same.
+  d_new$data %<>%
+    rename(
+      CASENO = `Case Number`,
+      VIOLATIONDATE = Date,
+      CHARGETITLE = Title,
+      CHARGESECTION = Section,
+      CHARGEPARA = Paragraph,
+      CHARGE = Violation,
+      VIOLATION_LOCATION = Address,
+      OFFICERDIV = Division
+    )
+
+  bundle_raw(
+    bind_rows(d_old$data, d_new$data),
+    c(d_old$loading_problems, d_new$data)
+  )
 }
 
 
@@ -38,8 +61,6 @@ clean <- function(d, helpers) {
   # TODO(phoebe): what is CHARGEPARA and CHARGESECTION? Can we get a data
   # dictionary?
   # https://app.asana.com/0/456927885748233/642528085814346
-	# TODO(phoebe): can we get beat/precinct or shapefiles for those?
-	# https://app.asana.com/0/456927885748233/642528085814347
   colnames(d$data) <- tolower(colnames(d$data))
   d$data %>%
   merge_rows(
@@ -47,7 +68,8 @@ clean <- function(d, helpers) {
     violation_location,
     officerdiv,
     race,
-    sex
+    sex,
+    caseno
   ) %>%
   rename(
     violation = charge,
@@ -70,10 +92,19 @@ clean <- function(d, helpers) {
     time = format(datetime, "%H:%M:%S"),
     subject_race = tr_race[raw_race],
     subject_sex = tr_sex[sex],
-    vehicle_year = format_two_digit_year(year)
+    vehicle_year = format_two_digit_year(year),
+    # NOTE: Data from 2018 on are all citations.
+    outcome = if_else(year(date) >= 2018, "citation", NA_character_)
   ) %>%
   helpers$add_lat_lng(
   ) %>%
+  helpers$add_shapefiles_data(
+  ) %>%
+  mutate(
+    beat = coalesce(beat, NAME.x),
+    division = DIVISION.y
+  )
+  debug_pipe() %>%
   helpers$add_type(
     "violation"
   ) %>%
